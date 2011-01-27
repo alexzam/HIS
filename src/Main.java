869abd2b71d41ddd240815711cf88c209f17e23b
AlexZam <alexzzam@gmail.com>
2011-01-28 00:32:26 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.metadata.ClassMetadata;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AlexZam
+ * Date: 24.01.11
+ * Time: 2:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            ourSessionFactory = new AnnotationConfiguration().
+                    configure("hibernate.cfg.xml").
+                    buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        try {
+            System.out.println("querying all the managed entities...");
+            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
+            for (Object key : metadataMap.keySet()) {
+                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
+                final String entityName = classMetadata.getEntityName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AlexZam
+  Date: 24.01.11
+  Time: 2:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head><title>Simple jsp page</title></head>
+<body>Place your content here</body>
+</html>
