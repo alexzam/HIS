9cfd9f73e532491fe0df020d0e6ccca24ea2718a
AlexZam <alexzzam@gmail.com>
2013-03-26 20:14:10 +0400

    Finished DB migration support.

--- src/main/java/az/his/dsmanager/DataSchemaManager.java
+++ src/main/java/az/his/dsmanager/DataSchemaManager.java
@@ -1,27 +1,33 @@
 package az.his.dsmanager;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class DataSchemaManager {
 
-    private static DataSource dataSource;
-    private static int dBVersionId;
+    private DataSource dataSource;
+    private int dBVersionId;
 
-    public static void init(String jndiName) throws NamingException {
+    public DataSchemaManager(String jndiName) throws NamingException {
         InitialContext context = new InitialContext();
         dataSource = (DataSource) context.lookup(jndiName);
     }
 
-    public static String getDBVersion() throws SQLException {
+    public String getDBVersion() throws SQLException {
         Connection connection = dataSource.getConnection();
         PreparedStatement statement
-                = connection.prepareStatement("SELECT value FROM sysParameters WHERE name = 'db.version'");
+                = connection.prepareStatement("SELECT val FROM sysParameters WHERE name = 'db.version'");
         ResultSet resultSet;
         try {
             resultSet = statement.executeQuery();
@@ -30,7 +36,7 @@ public class DataSchemaManager {
             resultSet.close();
             statement.close();
 
-            statement = connection.prepareStatement("SELECT value FROM sysParameters WHERE name = 'db.versionid'");
+            statement = connection.prepareStatement("SELECT val FROM sysParameters WHERE name = 'db.versionid'");
 
             resultSet = statement.executeQuery();
             if(!resultSet.first()) return "0";
@@ -42,10 +48,33 @@ public class DataSchemaManager {
         } catch (SQLException e) {
             dBVersionId = 0;
             return "0";
+        } finally {
+            connection.close();
         }
     }
 
-    public static int getDBVersionId() {
+    public int getDBVersionId() {
         return dBVersionId;
+    }
+
+
+    public void upgrade(List<Version> versions) {
+        Connection connection;
+        try {
+            connection = dataSource.getConnection();
+            ScriptRunner runner = new ScriptRunner(connection);
+
+            for (Version version : versions) {
+                InputStream stream = getClass().getClassLoader().getResourceAsStream("db/" + version.getFileName());
+                Reader reader = new InputStreamReader(stream);
+                runner.runScript(reader);
+                stream.close();
+            }
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/az/his/dsmanager/DataSchemaManagerFilter.java
+++ src/main/java/az/his/dsmanager/DataSchemaManagerFilter.java
@@ -1,23 +1,83 @@
 package az.his.dsmanager;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.servlet.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StreamCorruptedException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DataSchemaManagerFilter implements Filter {
     private String dbVersionStr;
     private int dbVersion;
     private int targetVersion;
 
+    boolean autoUpgradeMode = false;
+    private DataSchemaManager dataSchemaManager;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         String jndiName = filterConfig.getInitParameter("datasource");
 
         try {
-            DataSchemaManager.init(jndiName);
-            dbVersionStr = DataSchemaManager.getDBVersion();
-            dbVersion = DataSchemaManager.getDBVersionId();
+            dataSchemaManager = new DataSchemaManager(jndiName);
+            dbVersionStr = dataSchemaManager.getDBVersion();
+            dbVersion = dataSchemaManager.getDBVersionId();
             targetVersion = Integer.parseInt(filterConfig.getInitParameter("target-version"));
+
+            if (targetVersion > dbVersion) {
+                InputStream stream = getClass().getClassLoader().getResourceAsStream("db/migration.xml");
+
+                DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document document = documentBuilder.parse(stream);
+                stream.close();
+
+                NodeList configList = document.getElementsByTagName("config");
+                if(configList.getLength() > 1) throw new StreamCorruptedException("Migration file is corrupted.");
+
+                if(configList.getLength() > 0) {
+                    NodeList configNodes = configList.item(0).getChildNodes();
+                    for (int i = 0; i < configNodes.getLength(); i++) {
+                        Node item = configNodes.item(i);
+                        if(item.getNodeName().equals("auto-upgrade")){
+                            autoUpgradeMode = true;
+                        }
+                    }
+                }
+
+                NodeList versionList = document.getElementsByTagName("version");
+
+                List<Version> versions = new ArrayList<>();
+
+                for (int i = 0; i < versionList.getLength(); i++) {
+                    Node item = versionList.item(i);
+                    NamedNodeMap attributes = item.getAttributes();
+
+                    int verId = Integer.parseInt(attributes.getNamedItem("id").getNodeValue());
+                    if(verId <= dbVersion || verId > targetVersion) continue;
+
+                    versions.add(new Version(attributes.getNamedItem("name").getNodeValue(),
+                            verId,
+                            attributes.getNamedItem("upscript").getNodeValue()));
+                }
+
+                Collections.sort(versions);
+
+                if(autoUpgradeMode){
+                    dataSchemaManager.upgrade(versions);
+                    dbVersionStr = dataSchemaManager.getDBVersion();
+                    dbVersion = dataSchemaManager.getDBVersionId();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
