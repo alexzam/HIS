8a6094363dea574b08d6544104dbd4cf955eb314
AlexZam <alexzzam@gmail.com>
2013-06-28 10:48:55 +0400

    Continue standalone build.

--- HIS-standalone/src/main/java/az/his/App.java
+++ HIS-standalone/src/main/java/az/his/App.java
@@ -1,10 +1,12 @@
 package az.his;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
 import javax.servlet.ServletException;
 import java.io.File;
+import java.net.MalformedURLException;
 
 /**
  * Hello world!
@@ -12,20 +14,22 @@ import java.io.File;
  */
 public class App 
 {
-    public static void main( String[] args ) throws ServletException, LifecycleException {
-        String webappDirLocation = "webapp/";
+    public static void main( String[] args ) throws ServletException, LifecycleException, MalformedURLException {
+        String webappDirLocation = "../webapp/";
         Tomcat tomcat = new Tomcat();
 
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
+        tomcat.enableNaming();
+        tomcat.setBaseDir("tomcat");
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        Context context = tomcat.addWebapp("/his", new File(webappDirLocation).getAbsolutePath());
+        File configFile = new File("webapp/META-INF/context.xml");
+        context.setConfigFile(configFile.toURI().toURL());
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
         tomcat.start();
---
+++ HIS-standalone/src/main/resources/his.cmd
@@ -0,0 +1,2 @@
+cd classes
+java -Dfile.encoding=UTF-8 -classpath ".;../lib;../webapp/WEB-INF/lib" az.his.App
