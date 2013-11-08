6fb98a8908ab89f1e982782cc3421f6f1eb6d22b
AlexZam <alexzzam@gmail.com>
2013-11-08 11:58:15 +0400

    Continue making management screen.

--- src/main/webapp/js/az/AccountListScreen.js
+++ src/main/webapp/js/az/AccountListScreen.js
@@ -1,10 +1,20 @@
+Ext.define('accModel',{
+    extend:'Ext.data.Model',
+    fields: [
+        {name: 'name', type: 'string'},
+        {name: 'sum', type: 'string'}
+    ]
+});
+
 Ext.define('alexzam.his.AccountListScreen', {
     extend:'Ext.container.Viewport',
 
     requires:[
         'alexzam.his.view.toolbar.Toolbar',
 
-        'Ext.grid.Panel'
+        'Ext.grid.Panel',
+        'Ext.grid.column.Action',
+        'Ext.layout.container.Anchor'
     ],
     layout:'fit',
 
@@ -36,12 +46,32 @@ Ext.define('alexzam.his.AccountListScreen', {
                             text     : 'Сумма',
                             width    : 100,
                             dataIndex: 'sum'
+                        },
+                        {
+                            xtype:'actioncolumn',
+                            width:100,
+                            items: [
+                                {
+                                    icon: 'img/edit_button.png',
+                                    tooltip: 'Редактировать',
+                                    handler: function(grid,rowI, colI){
+                                        alert('edit-edit ' + rowI);
+                                    }
+                                }
+                            ]
                         }
                     ]
                 }
             ]
         }
     ],
+
+    store: Ext.create('Ext.data.Store', {
+       model: 'accModel',
+       data : [
+               {name: 'Test',    sum: '5000'}
+           ]
+       }),
 
     initComponent:function () {
         var me = this;
